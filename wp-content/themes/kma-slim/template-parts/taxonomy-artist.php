f4dc626fe29eecf488b87207c4f3fe2b50e10564
Bryan Baird <bryan@kerigan.com>
2017-12-28 15:52:15 -0500

    fixed

--- wp-content/themes/kma-slim/template-parts/taxonomy-artist.php
+++ wp-content/themes/kma-slim/template-parts/taxonomy-artist.php
@@ -18,11 +18,7 @@ $portfolio = new Portfolio();
 $profilePhoto = get_term_meta( $artist->term_id, 'artist_artist_photo', true );
 if($profilePhoto != ''){
     $photoInfo = pathinfo($profilePhoto);
-    if(file_exists($photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'])){
-        $artistPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'];
-    }else{
-        $artistPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-170x170.'.$photoInfo['extension'];
-    }
+    $artistPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'];
 }else{
     $profilePhoto = $portfolio->getWork( $artist->slug, [
         'posts_per_page' => 1,
@@ -76,11 +72,7 @@ $workTypes = $portfolio->getWorkTypes($artist);
 
                                 if(isset($type['work']['photo'])) {
                                     $photoInfo = pathinfo($type['work']['photo']);
-                                    if(file_exists($photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'])){
-                                        $newPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'];
-                                    }else{
-                                        $newPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-170x170.'.$photoInfo['extension'];
-                                    }
+                                    $newPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'];
                                 ?>
                                 <div class="column is-6-mobile is-12-tablet <?php echo $num; ?>">
                                     <figure class="artist-thumb-container is-1by1">
@@ -132,10 +124,10 @@ $workTypes = $portfolio->getWorkTypes($artist);
                             $i = 0;
                             foreach($work as $num => $piece){
                                 $photoInfo = pathinfo($piece['photo']);
-                                if(file_exists($photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'])){
-                                    $newPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'];
-                                }else{
+                                if(!file_exists($photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'])){
                                     $newPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-170x170.'.$photoInfo['extension'];
+                                }else{
+                                    $newPhoto = $photoInfo['dirname'].'/'.$photoInfo['filename'].'-300x300.'.$photoInfo['extension'];
                                 }
 
                                 $modalContent .= '<slide '.( $i==0 ? ':active="true"' : '' ).'>
